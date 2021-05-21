3893b541655e951b50738b4484ae6dcbde05a088
Rishabh Taparia <63252510+rt1301@users.noreply.github.com>
2021-05-22 02:16:42 +0530

    added leetcode problem 1109

---
+++ Leetcode/1109_corp_flightBook.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<int> corpFlightBookings(vector<vector<int>> &b, int n)
+    {
+        vector<int> a(n, 0);
+        for (int i = 0; i < b.size(); i++)
+        {
+            int first = b[i][0] - 1;
+            int last = b[i][1];
+            a[first] += b[i][2];
+            if (last < n)
+            {
+                a[last] -= b[i][2];
+            }
+        }
+        for (int i = 1; i < a.size(); i++)
+        {
+            a[i] += a[i - 1];
+        }
+        return a;
+    }
+};
